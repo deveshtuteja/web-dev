bb64661a7f0543d4de56b79aaec78b815614a7a1
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-08 15:34:45 +0530

    template literals

---
+++ JS/app1.js
@@ -0,0 +1,5 @@
+let pencilPrice=10;
+let eraserPrice=5;
+// let output="the total price is "+(pencilPrice+eraserPrice)+" rupees";
+// let output=`the total price is ${pencilPrice+eraserPrice} rupees`;
+console.log(`the total price is ${pencilPrice+eraserPrice} rupees`);
