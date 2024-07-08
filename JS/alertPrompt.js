50c801a1fd2f57557abaf9c4c58f5fc7dfcceb7d
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-08 22:19:19 +0530

    alerts and prompts

---
+++ JS/alertPrompt.js
@@ -0,0 +1,7 @@
+//ALERTS
+// alert("Something is wrong");
+// console.error("this is an error mssg");
+// console.warning("this is an warning mssg");
+
+let firstName=prompt("enter your name");
+console.log(firstName);
