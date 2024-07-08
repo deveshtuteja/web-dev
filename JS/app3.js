329019416a3324933e1d58ec691b446db94d3563
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-08 19:27:39 +0530

    if-else traffic light js

---
+++ JS/app3.js
@@ -0,0 +1,6 @@
+let firstName="Devesh";
+if(firstName=="Devesh") {
+    console.log(`Welcome ${firstName}`)
+} else {
+    console.log("didn't match")
+}
---
+++ JS/app4.js
@@ -0,0 +1,12 @@
+let color="red";
+
+//TRAFFIC LIGHT SYSTEM
+if(color==="red"){
+    console.log("STOP. Light color is red");
+}
+if(color==="yellow"){
+    console.log("PLEASE WAIT. Light color is yellow");
+}
+if(color==="green"){
+    console.log("GO. Light color is green");
+}
