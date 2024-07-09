4dab92d133fa37de92ab6b9a584c52dd9cbb25ff
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-09 12:57:47 +0530

    array in js

---
+++ JS/JS part 3/array.js
@@ -0,0 +1,12 @@
+let students=["Devesh","Yash","Jayant"];
+let info=["Devesh","20","Gurgaon"];
+let cars=["audi","bmw","xuv","maruti"];
+cars.push("toyota");
+cars.pop();
+
+//prac Q
+let months=["january","july","march","august"];
+months.shift();
+months.shift();
+months.unshift("june");
+months.unshift("july");
