b70211f58c62b40dec29e7e718d86632f0b13770
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-09 12:26:19 +0530

    string methods

---
+++ JS/JS part 3/app.js
@@ -0,0 +1,18 @@
+let mssg="    devesh tuteja     ";
+console.log(mssg);
+console.log(mssg.length);
+console.log(mssg.trim());
+
+let str="iLoveCoding";
+console.log(str.indexOf("Love"));
+console.log(str.replace("Love","Do"));
+
+let oldStr="  hello  ";
+console.log(oldStr.trim().toUpperCase());
+
+let str1="devesh";
+console.log(str1.slice(1,4));
+
+let str2="workhard";
+console.log(str2.slice(-2));
+console.log(str2.repeat(3));
