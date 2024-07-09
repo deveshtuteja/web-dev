c80c04ddea4acbdb0be0298370731aa80b16a134
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-09 16:32:46 +0530

    assignment ques arrays

--- JS/JS part 3/array.js
+++ JS/JS part 3/array.js
@@ -16,3 +16,10 @@ months.splice(0,2,"july","june");
 
 //tic-tac-toe game state using nested arrays
 let ticTactoe=[['X',null,'O'],[null,'X',null],['O',null,'X']];
+
+//ASSIGNMENT QUES
+let arr=[7,9,0,-2];
+//Q1
+// arr.splice(-1);
+//Q2
+arr.shift();
