c4da0ec19b9023670362cdcd52b8af74026c5801
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-10 13:30:44 +0530

    GUESS GAME JS

---
+++ JS/JS part 5/guessGame.js
@@ -0,0 +1,20 @@
+const max=prompt("enter a max numer:");
+console.log(`Max number is ${max}`);
+let random= Math.floor(Math.random()*max+1);
+let guess=prompt("make a guess:");
+while(true){
+    if(guess=="quit"){
+        console.log("quitting the game");
+        break;
+    }
+
+    
+    if(guess==random){
+        console.log("Congratulations! The random no. was",random);
+        break;
+    } else if(guess>random){
+        guess=prompt("HINT:-The no. you entered is larger. Try again!");
+    } else {
+        guess=prompt("HINT:-The no. you entered is smaller. Try again!");
+    }
+}
