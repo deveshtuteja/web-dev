4bbfb41d56d2afeca0726ce1ef444ea481b3ce23
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-11 22:33:59 +0530

    reduce & spread

--- JS/JS part 8/app.js
+++ JS/JS part 8/app.js
@@ -38,3 +38,53 @@ console.log(even);
 //Every and Some
 console.log(nums.every((el)=>el%2==0));
 console.log(nums.some((el)=>el%2==0));
+
+//Reduce
+let num=[1,2,3,4];
+let finalVal=num.reduce((res,el)=>res+el);
+console.log(finalVal);
+//max in an array using reduce
+let n=[1,4,5,9,21,3,5,9,0];
+let max=n.reduce((max,el)=>{
+    if(el>max){
+        return el;
+    } else {
+        return max;
+    }
+});
+console.log(max);
+//min in an array
+let min=n.reduce((min,el)=>{
+    if(el<min){
+        return el;
+    } else {
+        return min;
+    }
+});
+console.log(min);
+//mult of 10
+console.log(n.every((el)=>el%10==0));
+
+//SPREAD
+console.log(...arr);
+console.log(...nums);
+console.log(..."Devesh Tuteja");
+
+//spread(with array literals)
+let eve=[2,4,6,8,10];
+let odd=[1,3,5,7,9];
+let allNum=[...eve,...odd];
+console.log(allNum);
+
+//spread(with obj literals)
+const data={
+    email:"123@hotmail.com",
+    pwd:"abcd"
+};
+const dataCopy={...data,id:123};
+let obj1={...odd}; //conv array to object
+//key:value pairs->key=index and value=arr[i]
+//*SAME IN STRINGS*//
+
+
+
