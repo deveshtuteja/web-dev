4a765301454390469c3b395ea8c128e8802b2348
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-12 23:00:27 +0530

    keyboard events

--- JS/JS part 10/app.js
+++ JS/JS part 10/app.js
@@ -18,20 +18,46 @@
 //         console.log("You double clicked me!");
 //     })
 // }
-let h1=document.querySelector("h1");
-let h3=document.querySelector("h3")
-let p=document.querySelector("p");
-let btn=document.querySelector("button");
+// let h1=document.querySelector("h1");
+// let h3=document.querySelector("h3")
+// let p=document.querySelector("p");
+// let btn=document.querySelector("button");
 
-function colorBlue(){
-    console.log(this.textContent);
-    this.style.backgroundColor="blue";
-}
+// function colorBlue(){
+//     console.log(this.textContent);
+//     this.style.backgroundColor="blue";
+// }
 
-btn.addEventListener("click",colorBlue);
+// btn.addEventListener("click",function(event){
+//     console.log(event);
+//     console.log("mouse is clicked");
+// });
+// btn.addEventListener("dblclick",function(event){
+//     console.log(event);
+//     console.log("mouse is double-clicked");
+// });
 
-h1.addEventListener("click",colorBlue);
+// h1.addEventListener("click",colorBlue);
 
-h3.addEventListener("click",colorBlue);
+// h3.addEventListener("click",colorBlue);
 
-p.addEventListener("click",colorBlue);
+// p.addEventListener("click",colorBlue);
+
+let input=document.querySelector("input");
+input.addEventListener("keydown",function(event){
+    console.log("code:",event.code);
+    if(event.code=="ArrowUp"){
+        console.log("Characters moves forward");
+    } else if(event.code=="ArrowDown"){
+        console.log("Characters moves downward")
+    }else if(event.code=="ArrowRight"){
+        console.log("Characters moves right")
+    }else if(event.code=="ArrowLeft"){
+        console.log("Characters moves left")
+    } else {
+        console.log("Wrong key pressed");
+    }
+})
+// input.addEventListener("keyup",function(){
+//     console.log("key was released");
+// })
