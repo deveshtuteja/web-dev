f680dce82dd415a61fc4f3294034e3abdd243813
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-15 12:20:15 +0530

    callback hell-js

---
+++ JS/JS part 11/app.js
@@ -0,0 +1,21 @@
+h1=document.querySelector("h1");
+
+function changeColor(color,delay,nextColorChange) {
+    setTimeout(()=>{
+        h1.style.color=color;
+        if(nextColorChange){
+            nextColorChange();
+        }
+    },delay);
+};
+
+changeColor("red",1000,()=>{
+    changeColor("orange",1000,()=>{
+        changeColor("green",1000,()=>{
+            changeColor("yellow",1000,()=>{
+                changeColor("blue",1000);
+            });
+        });
+    });
+});
+//CALLBACK HELL->nesting of functions
