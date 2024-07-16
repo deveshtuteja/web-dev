d4e9c070a84018b525910a0c3bcafafba14266cb
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-16 22:32:17 +0530

    react set up-create & vite

---
+++ React js/01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | Devesh Tuteja</h1>
+  );
+}
+
+export default App;
---
+++ React js/01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+      <h1>Chai aur React with vite| Devesh Tuteja</h1>
+  )
+}
+
+export default App
