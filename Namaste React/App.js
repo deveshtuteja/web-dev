18be60aea5bd4de01d23f0d69d595838b51066c0
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-19 23:10:20 +0530

    using react without jsx

--- Namaste React/App.js
+++ Namaste React/App.js
@@ -1,13 +1,46 @@
-const heading = React.createElement(
-    "h1", 
-    {
-    id:"heading",
-    xyz:"abc"
-}, 
-"hello world from react");
+/**
+ * <div>
+ *    <div>
+ *          <h1> I'm a h1 tag </h1>
+ *          <h2> I'm a h2 tag </h2>
+ *    </div>
+ *    <div>
+ *          <h1> I'm a h1 tag </h1>
+ *          <h2> I'm a h2 tag </h2>
+ *    </div>
+ *  </div>
+ */
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement(
+            "h1", {}, "I'm an h1 tag"
+        ), React.createElement(
+            "h2", {}, "I'm an h2 tag"
+        )]
+    ), React.createElement(
+        "div",
+        { id: "child2" },
+        [React.createElement(
+            "h1", {}, "I'm an h1 tag"
+        ), React.createElement(
+            "h2", {}, "I'm an h2 tag"
+        )]
+    )]
+)
+// const heading = React.createElement(
+//     "h1",
+//     {
+//         id: "heading",
+//         xyz: "abc"
+//     },
+//     "hello world from react");
 
-console.log(heading); //OBJECT
+console.log(parent); //OBJECT
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
