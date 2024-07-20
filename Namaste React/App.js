73565233f7d8cc217dac9c420b3aa014944d94a3
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-20 21:54:02 +0530

    Igniting our app. Parcel complete concept.

--- Namaste React/App.js
+++ Namaste React/App.js
@@ -1,15 +1,6 @@
-/**
- * <div>
- *    <div>
- *          <h1> I'm a h1 tag </h1>
- *          <h2> I'm a h2 tag </h2>
- *    </div>
- *    <div>
- *          <h1> I'm a h1 tag </h1>
- *          <h2> I'm a h2 tag </h2>
- *    </div>
- *  </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     { id: "parent" },
@@ -17,7 +8,7 @@ const parent = React.createElement(
         "div",
         { id: "child" },
         [React.createElement(
-            "h1", {}, "I'm an h1 tag"
+            "h1", {}, "I'm an h1 tag 🚀"
         ), React.createElement(
             "h2", {}, "I'm an h2 tag"
         )]
