93861d842b5352e3f443fa8d2d4be502b743b2e2
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-21 17:13:51 +0530

    JSX, babel, React component

--- Namaste React/App.js
+++ Namaste React/App.js
@@ -10,9 +10,29 @@ console.log(heading);
 //JSX => babel transpiles it into React.createElement => JS OBJECT => Render() => HTMLElement
 
 //JSX-> HTML-like syntax
-const jsxHeading= <h1 id="heading" className="head">Namaste React using JSX🚀</h1>
+const jsxHeading = (
+<h1 id="heading" className="head">
+    Namaste React using JSX🚀
+</h1>
+);
 console.log(jsxHeading);
+
+const Title=()=>
+    <h1>Devesh Tuteja</h1>
+
+//REACT COMPONENT
+// const HeadingComponent = () => {
+//     return <h1 className="heading"> Namaste React FUNCTIONAL COMPONENT </h1>;
+// }
+//these two are same
+
+//Component Composition -> Components inside Components
+const HeadingComponent = () => 
+<div id="container">
+    <Title/>
+    <h1 className="heading"> Namaste React FUNCTIONAL COMPONENT </h1>
+</div>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
