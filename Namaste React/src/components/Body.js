78cf9c6fff49c7a99e0f68bbe837975025b5d0d7
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-22 22:36:39 +0530

    use Effect, Swiggy API Call, Shimmer UI

--- Namaste React/src/components/Body.js
+++ Namaste React/src/components/Body.js
@@ -1,12 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
     //local state var->super powerful react var
-    const [listOfRes, setListOfRes] = useState(resList);
+    const [listOfRes, setListOfRes] = useState([]);
     //normal JS var
     // let listOfRes=[];
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    const fetchData= async ()=>{
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4738044&lng=77.01067610000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        console.log(json);
+        //Optional Chaining
+        setListOfRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+    //Conditional Rendering
+    if(listOfRes.length===0){
+        return <Shimmer />
+    }
 
     return (
         <div className="body">
