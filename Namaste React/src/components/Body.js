91580f8a775226ef33a2503882bca7607f46011a
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-22 19:29:13 +0530

    use State-food delivery app

--- Namaste React/src/components/Body.js
+++ Namaste React/src/components/Body.js
@@ -1,17 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {resList.map((restaurant) => (
-            <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
-          ))}
-        </div>
-      </div>
-    );
-  };
+    //local state var->super powerful react var
+    const [listOfRes, setListOfRes] = useState(resList);
+    //normal JS var
+    // let listOfRes=[];
 
-  export default Body;
+    return (
+        <div className="body">
+            <div className="filter">
+                <button className="filter-btn"
+                    onClick={() => {
+                        const filteredList = listOfRes.filter((res) => res.info.avgRating > 4.5);
+                        setListOfRes(filteredList);
+                    }}>Top Rated Restaurants</button>
+            </div>
+            <div className="res-container">
+                {listOfRes.map((restaurant) => (
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Body;
