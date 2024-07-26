15b907a12b7dc7bd3574366fbeb1c0588c22fefa
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-26 21:47:36 +0530

    lifecycle of methods & nested class in class based components

--- Namaste React/src/components/About.js
+++ Namaste React/src/components/About.js
@@ -1,13 +1,26 @@
-import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
-const About=()=>{
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-            <User name="Devesh Tuteja (function)" />
-            <UserClass name="Devesh Tuteja (class)" location="Gurgaon(class)" />
-        </div>
-    );
-};
-export default About
+
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+
+        console.log("Parent Constructor");
+    }
+    componentDidMount(){
+        console.log("Parent component did mount");
+    }
+    render() {
+        console.log("Parent Render");
+        return (
+            <div>
+                <h1>About Class Component</h1>
+                <h2>This is Namaste React Web Series</h2>
+                <UserClass name="Devesh Tuteja (class)" location="Gurgaon(class)" />
+            </div>
+        )
+    };
+}
+
+export default About;
--- Namaste React/src/components/UserClass.js
+++ Namaste React/src/components/UserClass.js
@@ -3,13 +3,30 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
-  }
+    
+    this.state = {
+      count: 0,
+    };
 
+    console.log("child constructor is called");
+  }
+  componentDidMount(){
+    console.log("Child component did mount");
+  }
   render() {
-    const {name,location}=this.props;
+    const { name, location } = this.props;
+    const { count } = this.state;
+
+    console.log("child render is called...");
+
     return (
       <div className="user-card">
+        <h1>Count = {count}</h1>
+        <button onClick={() => {
+          this.setState({
+            count: this.state.count + 1
+          });
+        }}>Count Increment</button>
         <h2>{name}</h2>
         <h2>{location}</h2>
         <h2>Contact: @deveshtuteja</h2>
@@ -19,4 +36,5 @@ class UserClass extends React.Component {
 }
 
 export default UserClass;
+
 
