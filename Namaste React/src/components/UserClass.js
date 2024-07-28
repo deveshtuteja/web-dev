fd0db644edbfee8f2d9c65b239ec835a7433eb6e
deveshtuteja <deveshtuteja17@gmail.com>
2024-07-28 21:26:29 +0530

    GitHub API, lifecycle-updating & unmouting

--- Namaste React/src/components/UserClass.js
+++ Namaste React/src/components/UserClass.js
@@ -3,27 +3,47 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      
+      userInfo: {
+        name: "dummy-name",
+        location: "dummy-loc",
+        avatar_url: "/avatar-url",
+      }
     };
 
-    // console.log(this.props.name+" constructor is called");
+    console.log(this.props.name+" constructor is called");
   }
-  componentDidMount(){
-    // console.log(this.props.name+" component did mount");
-  }
-  render() {
-    const { name, location } = this.props;
-    const { count } = this.state;
+  async componentDidMount() {
+    console.log(this.props.name+" component did mount");
+    //API calls
+    const userData = await fetch("https://api.github.com/users/akshaymarch7");
+    const json = await userData.json();
+    console.log(json);
 
-    // console.log(this.props.name+" render is called...");
+    this.setState({
+      userInfo: json
+    });
+  }
+
+  componentDidUpdate(){
+    console.log("Component Did Update is called");
+  }
+
+  componentWillUnmount(){
+    console.log("Component Will Unmount is called....");
+  }
+
+  render() {
+    const { name, location, avatar_url } = this.state.userInfo;
+    console.log(this.props.name+" render is called...");
 
     return (
       <div className="user-card">
+        <img src={avatar_url} />
         <h2>{name}</h2>
         <h2>{location}</h2>
-        <h2>Contact: @deveshtuteja</h2>
+        <h2>Contact: @akshaymarch7</h2>
       </div>
     );
   }
