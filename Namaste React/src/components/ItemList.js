7c7ec856e4ffb56ccc32e9c8ca1fff4de66f55ac
deveshtuteja <deveshtuteja17@gmail.com>
2024-08-02 23:05:05 +0530

    menu add item ui improved

--- Namaste React/src/components/ItemList.js
+++ Namaste React/src/components/ItemList.js
@@ -1,14 +1,22 @@
 import { useDispatch } from "react-redux";
 import { addItems } from "../utils/cartSlice";
 import { ITEM_URL } from "../utils/constants";
+import { useState } from "react";
 
 const ItemList = ({ items }) => {
-
+    const [activeItemId, setActiveItemId] = useState(null);
     const dispatch = useDispatch();
+
     const handleAddItem = (item) => {
-        //dispatch an action
+        // Dispatch an action
         dispatch(addItems(item));
-    }
+
+        // Set the active item ID and reset after a delay
+        setActiveItemId(item.card.info.id);
+        setTimeout(() => {
+            setActiveItemId(null);
+        }, 500); // Adjust the duration (in milliseconds) as needed
+    };
 
     return (
         <div>
@@ -16,7 +24,7 @@ const ItemList = ({ items }) => {
                 <div
                     data-testid="foodItems"
                     key={item.card.info.id}
-                    className="m-2 p-2 border-gray-200 border-b-2 text-left flex justify-between items-center"
+                    className={`m-2 p-2 border-gray-200 border-b-2 text-left flex justify-between items-center ${activeItemId === item.card.info.id ? 'bg-green-500' : ''}`}
                 >
                     <div className="w-9/12">
                         <div className="py-2 font-semibold">
@@ -28,12 +36,10 @@ const ItemList = ({ items }) => {
                         </div>
                     </div>
                     <div className="w-3/12 flex flex-col items-center">
-                        {/* Adjusting the size and appearance of the image */}
                         <img
                             src={ITEM_URL + item.card.info.imageId}
                             className="w-24 h-24 object-cover rounded-md mb-2"
                         />
-                        {/* Styling the button with better visual appearance and interactivity */}
                         <button
                             className="p-2 bg-white text-green-500 border border-green-500 rounded-sm hover:bg-green-500 hover:text-white transition"
                             onClick={() => handleAddItem(item)}
@@ -44,7 +50,7 @@ const ItemList = ({ items }) => {
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
 export default ItemList;
